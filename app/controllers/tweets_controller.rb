480e053868bda2b6f6aee2037aadcdb46e5ffdeb
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-08-03 09:16:23 +0000

    Defined the actions for tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,37 @@
 class TweetsController < ApplicationController
+  
+  def index
+    @tweets = Tweet.includes(:user).with_attached_images
+  end
+  
+  def create
+    Tweet.create(update_params)
+  end
+  
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    Tweet.update(update_params)
+  end
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+  
+  private
+  def update_params
+    params.require(:tweet).permit(:text, images: []).merge(user_id: current_user.id)
+  end
+  
 end
