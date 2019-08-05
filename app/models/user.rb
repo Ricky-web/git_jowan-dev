ff3417944e98c51749c7e81ee87736ac24b56bcc
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-08-05 08:27:55 +0000

    Fixed devise's views and the validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:nickname]
+  
+  validates :nickname, 
+  presence: true, uniqueness: true, 
+  length: {maximum: 8}, format: { with: /\A[a-z0-9]+\z/i }
          
   has_many :tweets
   
