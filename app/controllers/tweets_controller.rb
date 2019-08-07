54cb3d70771c35b52e67b222ccd31dcbb124a793
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-08-07 03:57:07 +0000

    Fixed validation for tweet model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,31 @@
 class TweetsController < ApplicationController
+  layout 'form', only: [:new, :edit]
+  before_action :user_confirm, only: [:new]
   
   def index
     @tweets = Tweet.includes(:user).with_attached_images
   end
   
   def create
-    Tweet.create(update_params)
+    tweet = Tweet.new(update_params)
+    
+    if tweet.valid?
+      Tweet.create(update_params)
+      redirect_to :root
+    else
+      @tweet = Tweet.new
+      @tweet.text = update_params[:text]
+      
+      alerts = tweet.errors.full_messages
+      flash.now[:alert] = []
+      
+      alerts.each do |alert|
+        flash.now[:alert] << "#{alert}"
+      end
+      
+      render "new.html.erb", 
+      locals: {tweet: @tweet}, layout: 'form'
+    end
   end
   
   def new
@@ -34,4 +54,11 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:text, :currency_pair, images: []).merge(user_id: current_user.id)
   end
   
+  def user_confirm
+    if user_signed_in?
+      true
+    else
+      redirect_to new_user_session_path, alert: "If you want to create your tweet, please sign in!"
+    end
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,8 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   
+  [:currency_pair, :images, :text].each do |v|
+    validates v, presence: true
+  end
+
 end
