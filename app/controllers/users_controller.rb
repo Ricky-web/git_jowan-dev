481ff3a7aa562fee2c07b7be36f81a0f6a81320e
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-08-08 07:55:10 +0000

    Created users controller and Defined actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user_profile = User.find(params[:id]).includes(:tweets)
+  end
+  
+  def edit
+    @user_profile = User.find(params[:id])
+  end
+  
+  def update
+    user_profile = Tweet.find[:id]
+    user_profile.update(update_params)
+  end
+  
+  private
+  def update_params
+    params.require(:users).permit(:nickname, :age, :gender, :nationality, :occupation, :self_introduction)
+  end
+end
