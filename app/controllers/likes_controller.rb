bde0a51d7f646837f94daea3cbf4e8a6817a326f
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-08-14 09:03:16 +0000

    Defined actions for likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  
+  def create
+    @like = current_user.likes.create(params[:id])
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, tweets_id: params[:id])
+    @like.destroy
+    redirect_back(fallback_location: root_path)
+  end
+  
 end
