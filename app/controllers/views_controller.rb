8a3813edd1dfd54dfc2e1b0ca90b7e986c203682
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-08-14 12:40:37 +0000

    Fixed create action for views controller

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,5 +1,6 @@
 class ViewsController < ApplicationController
   def create
     View.create(tweet_id: params[:tweet_id], user_id: current_user.id)
+    redirect_to controller: 'tweets', action: 'show', id: "#{params[:tweet_id]}"
   end
 end
