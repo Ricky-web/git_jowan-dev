1858623a6a5d8c4082825953b06e4e44bebc8f24
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-08-14 12:48:26 +0000

    Defined the assciation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,7 @@ class Tweet < ApplicationRecord
   has_many :comments
   has_many_attached :images
   has_many :likes
+  has_many :views
   has_many :liked_users, through: :likes, source: :user
   
   [:title, :currency_pair, :images, :text].each do |v|
