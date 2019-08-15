5ce72be35b15ab0dcaa76054a0780dae3a90952f
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-08-15 08:02:49 +0000

    Fixed views

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,6 +1,8 @@
 class ViewsController < ApplicationController
+  
   def create
     View.create(tweet_id: params[:tweet_id], user_id: current_user.id)
     redirect_to controller: 'tweets', action: 'show', id: "#{params[:tweet_id]}"
   end
+  
 end
